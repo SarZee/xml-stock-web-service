e7f7590c33237e40921b056c905c3448e21e6302
SarZee <120094635+SarZee@users.noreply.github.com>
2023-11-12 18:58:04 +0000

    Project files

--- scripts/router.js
+++ scripts/router.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('<h1>Welcome to Stock Web Service!</h1>')
+});
+
+router.get('/add-item', (req, res) => {
+  res.send('<h1>This is add-item service.</h1>')
+});
+
 router.post('/add-item', (req, res) => {
     
 });

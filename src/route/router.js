b4a7ed652410e51a08317fe690e96acfa89ad27c
SarZee <thorntirawit4800@gmail.com>
2023-11-19 22:51:32 +0700

    Project files

---
+++ src/route/router.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/add-item', (req, res) => {
+
+});
+
+router.post('/update-item', (req, res) => {
+
+});
+
+router.post('/add-shipping-cost', (req, res) => {
+
+});
+
+router.post('/add-order', (req, res) => {
+
+});
+
+router.post('/confirm-order/#orderID', (req, res) => {
+
+});
+
+router.post('/request-cancel-order/#orderID', (req, res) => {
+
+});
+
+router.post('/cancel-order/#orderID', (req, res) => {
+
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const server = express();
 const port = 8000;
+const router = require('./route/router');
 
 const soap = require('soap');
 const path = '/stock-service';
 const service = require('./soap/service');
 const wsdl = require('fs').readFileSync('./wsdl/stock-service.wsdl', 'utf-8');
+
+server.use('stock-service/', router);
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}.`);

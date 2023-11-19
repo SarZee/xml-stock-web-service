9d088027354ff73f147bb0aaf1bf112001e6a477
SarZee <thorntirawit4800@gmail.com>
2023-11-19 18:01:18 +0700

    Project files

--- src/server.js
+++ src/server.js
@@ -7,9 +7,6 @@ const path = '/stock-service';
 const service = require('./soap/service');
 const wsdl = require('fs').readFileSync('./wsdl/stock-service.wsdl', 'utf-8');
 
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./database/stock.db');
-
 server.listen(port, () => {
     console.log(`Server listening on port ${port}.`);
     soap.listen(server, path, service, wsdl, () => {

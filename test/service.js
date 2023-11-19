bf5bcf0ee7704776dbe1b2e9ee7e73be7d8354a3
SarZee <thorntirawit4800@gmail.com>
2023-11-19 18:01:18 +0700

    Project files

--- test/service.js
+++ test/service.js
@@ -4,10 +4,9 @@ const wsdl = 'http://localhost:8000/stock-service?wsdl';
 
 soap.createClient(wsdl, (err, client) => {
     if (err) {
-        console.error(`SOAP client creation error: ${err.message}`);
+        console.error(`Client error: ${err.message}`);
     } else {
-        console.log(client.describe());
-
+        console.log(`Services list: `);
         console.log(client.StockService.StockPort);
     }
 });
